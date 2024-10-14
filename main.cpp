bca5b893f8197b2dc918af5105653fe1a08789b1
Gabriel <gabriel.rouge@etu.univ-amu.fr>
2024-10-14 17:29:18 +0200

    Tentative de deadlock

--- main.cpp
+++ main.cpp
@@ -6,61 +6,86 @@
 
 using namespace std;
 
+// VARIABLES GLOBALES
+
 int patients = 0; // Nombre de clients dans la clinique
 bool medicIsSleeping; // True si le docteur dort
+vector<thread> patientsThreads;
 
-int n = 5; // Nombre de chaises
-int tmp_takeCareOfPatient = 5; // Temps que prend le médecin pour s'occuper d'un patient
+// PARAMETRES
+
+int n = 3; // Nombre de chaises
+int tmp_takeCareOfPatient = 1; // Temps que prend le médecin pour s'occuper d'un patient
 int tmp_newPatient = 2; // Intervalle de temps entre chaque nouveau patient
+
+// RESSOURCES CRITIQUES
+mutex mut;
+
+void takeCareOfPatient()
+{
+    cout << "Le médecin s'occupe d'un patient\n";
+    sleep(tmp_takeCareOfPatient);
+}
+
+void medicNap()
+{
+    medicIsSleeping = true;
+
+    cout << "Le médecin fait la sieste\n";
+    while(medicIsSleeping)
+    {
+        sleep(1);
+    }
+    cout << "Le médecin se réveille\n";
+
+    return;
+}
 
 void medicRoutine()
 {
     while(true)
     {
+        mut.lock();
         if(patients == 0) // Si il n'y a pas de patient
         {
-            medicIsSleeping = true;
-            cout << "Le médecin fait la sieste\n";
-            while(medicIsSleeping)
-            {
-                sleep(1);
-            }
-            cout << "Le médecin se réveille\n";
+            medicNap();
         }
         else
         {
-            cout << "Le médecin s'occupe d'un patient\n";
-            sleep(tmp_takeCareOfPatient);
-            patients -= 1; // Et le patient sort
+            takeCareOfPatient();
+            patients -= 1;
         }
+        mut.unlock();
     }
+}
 
+void wakeUpMedic()
+{
+    cout << "Le patient " << this_thread::get_id() << " réveille le médecin\n";
+    medicIsSleeping = false;
 }
 
 void patientRoutine()
 {
-    cout << "Le patient " << this_thread::get_id() << " rentre dans la clinique\n";
+    mut.lock();
     // Le patient arrive dans la clinique
     if(patients == n)
     {
         cout << "La clinique ne peut pas accueillir le patient " << this_thread::get_id() << endl;
-        cout << "Le patient " << this_thread::get_id() << " sort de la clinique\n";
         return;
     }
 
     if(patients == 0 && medicIsSleeping)
     {
-        cout << "Le patient " << this_thread::get_id() << " réveille le médecin\n";
-        medicIsSleeping = false; // Si la file est vide et le docteur dort, le patient réveille le doteur
+        wakeUpMedic();
     }
 
     // Le patient attend son tour
     cout << "Le patient " << this_thread::get_id() << " va dans la file d'attente\n";
     patients += 1;
     cout << patients << " patients dans la file d'attente\n";
+    mut.unlock();
 }
-
-vector<thread> patientsThreads;
 
 int main()
 {
