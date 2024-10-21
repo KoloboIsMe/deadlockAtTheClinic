57b748f8a2837a1e43ab983c6244fbafef231a05
Gabriel <gabriel.rouge@etu.univ-amu.fr>
2024-10-21 16:39:24 +0200

    Deadlock !

--- main.cpp
+++ main.cpp
@@ -5,45 +5,72 @@
 #include <chrono>
 using namespace std;
 
-int chrono_newPatient = 2;
+int chrono_newPatient = 1;
 
-int chairs = 1;
+int chairs = 5;
 int customers = 0;
 mutex mut;
 mutex customer;
 mutex barber;
 mutex customerDone;
 mutex barberDone;
+mutex barberWakeUp;
+
+
+void balk(int id){
+    cout << "Patient " << id << ": s'en va car la clinique est pleine" << endl;
+    while(true){}
+}
 
 void getHairCut(int id)
 {
+    // Arrivée dans la clinique
+    cout << "Patient " << id << ": entre dans la clinique" << endl;
+
     mut.lock();                 // Vérouille le compteur de clients
-    if(customers == chairs)     // Si la salle est pleine
+    if(customers >= chairs)     // Si la salle est pleine
     {                           //
-        mut.unlock();           // Libère le compteur de clients et s'en va.
+        mut.unlock();           //      Libère le compteur de clients
+        balk(id);               //      Sort de la clinique
     }                           //
-    else                        // Sinon
-    {                           //
-        customers++;            // Rentre dans le magasin
-        customer.lock();        // Vérouille la ressource critique client ?
-        barber.lock();          // Vérouille la ressource critique barbier ?
-        // Get the haircut      // Se fait couper les cheveux
-        customerDone.lock();    // Vérouille la ressource critique customerDone ?
-        barberDone.lock();      // Vérouille la ressource critique barberDone ?
-        mut.lock();             // Vérouille le compteur de clients
-        customers--;            // Sort du magasin
-        mut.unlock();           // Libère le compteur de clients
+    customers++;                // Rentre dans le magasin
+    mut.unlock();               // Dévérouille le compteur de clients
+
+    // Attente et consultation
+    cout << "Patient " << id << ": attend pour sa consultation" << endl;
+    if (customers == 1 && barberWakeUp.try_lock() == false) {
+        barberWakeUp.unlock();
     }
 
+    customer.unlock();          // Déclare qu'il est disponible
+    barber.lock();              // Attend que le médecin soit disponible
+    // Get the haircut          // Se fait couper les cheveux
+    barberDone.lock();          // Attend que le médecin déclare aussi qu'il a terminé
+    customerDone.unlock();      // Déclare qu'il a fini sa consultation
+
+    // Sortie de la clinique
+    cout << "Patient " << id << ": sort de la clinique" << endl;
+
+    mut.lock();                 // Vérouille le compteur de clients
+    customers--;                // Sort de la clinique
+    mut.unlock();               // Libère le compteur de clients
 }
 
 void cutHair()
 {
-    customer.lock();            // Vérouille la ressource critique client ?
-    barber.lock();              // Vérouille la ressource critique barbier ?
+    cout << "Médecin : Attend un patient" << endl;
+
+    if(customer.try_lock() == false) { // Si il n'y a personne dans la file d'attente
+        cout << "Médecin : S'endort car il n'y a pas de patient" << endl;
+        barberWakeUp.lock();    // Le barbier s'endort
+        barberWakeUp.lock();    // Attend qu'on le réveil
+        cout << "Médecin : Se fait réveiller" << endl;
+    }
+    barber.unlock();            // Déclare qu'il est disponible
+    customer.lock();            // Attend un client
     // Coupe les cheveux        // Coupe les cheveux du client
-    customerDone.lock();        // Vérouille la ressource critique customerDone
-    barberDone.lock();          // Vérouille la ressource critique barberDone ?
+    customerDone.lock();        // Attend que le client déclare qu'il a fini
+    barberDone.unlock();        // Termine son travail
 }
 
 void routine()
@@ -61,6 +88,7 @@ int main()
     int clientId = 1;
 
     while (true) {
+        cout << "Nombre de patients dans la clinique :"<< customers << endl;
         patientsThreads.push_back(thread(getHairCut, clientId++));
         sleep(chrono_newPatient);
     }
