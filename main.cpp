8e2001ff08d3a38ed2bd5fe68ff37efeaf0c31d0
Gabriel <rougegabriel2@gmail.com>
2024-10-21 22:06:30 +0200

    Traduction sur le sujet de clinique

--- main.cpp
+++ main.cpp
@@ -2,19 +2,19 @@
 #include <mutex>
 #include <thread>
 #include <vector>
-#include <chrono>
 using namespace std;
 
 int chrono_newPatient = 1;
 
 int chairs = 5;
-int customers = 0;
-bool barberAsleep = false;
+int patients = 0;
 mutex mut;
-mutex customer;
-mutex barber;
-mutex customerDone;
-mutex barberDone;
+mutex patient;
+mutex medic;
+mutex patientDone;
+mutex medicDone;
+mutex medicWakeUp;
+bool medicAsleep;
 
 
 void balk(int id){
@@ -22,81 +22,78 @@ void balk(int id){
     while(true){}
 }
 
-void getHairCut(int id)
+void visitClinic(int id)
 {
     // Arrivée dans la clinique
     cout << "Patient " << id << ": entre dans la clinique" << endl;
 
-    mut.lock();                 // Vérouille le compteur de clients
-    if(customers >= chairs)     // Si la salle est pleine
+    mut.lock();                 // Vérouille le compteur de patients
+    if(patients >= chairs)      // Si la salle est pleine
     {                           //
-        mut.unlock();           //      Libère le compteur de clients
+        mut.unlock();           //      Libère le compteur de patients
         balk(id);               //      Sort de la clinique
     }                           //
-    customers++;                // Rentre dans le magasin
-    mut.unlock();               // Dévérouille le compteur de clients
+    patients++;                 // Rentre dans la clinique
+    mut.unlock();               // Dévérouille le compteur de patients
 
-    if (customers == 1 && barberAsleep) {
+    if (patients == 1 && medicWakeUp.try_lock() == false) {
         cout << "Patient " << id << ": réveille le médecin" << endl;
-        barberAsleep = false;
+        medicAsleep = false;
     }
 
     // Attente et consultation
     cout << "Patient " << id << ": attend pour sa consultation" << endl;
 
-    customer.unlock();          // Déclare qu'il est disponible
-    barber.lock();              // Attend que le médecin soit disponible
-    // Get the haircut          // Se fait couper les cheveux
-    barberDone.lock();          // Attend que le médecin déclare aussi qu'il a terminé
-    customerDone.unlock();      // Déclare qu'il a fini sa consultation
+    patient.unlock();           // Déclare qu'il est disponible
+    medic.lock();               // Attend que le médecin soit disponible
+                                // Consultation ...
+    medicDone.lock();           // Attend que le médecin déclare aussi qu'il ait terminé
+    patientDone.unlock();       // Déclare qu'il a fini sa consultation
 
     // Sortie de la clinique
     cout << "Patient " << id << ": sort de la clinique" << endl;
 
-    mut.lock();                 // Vérouille le compteur de clients
-    customers--;                // Sort de la clinique
+    mut.lock();                 // Vérrouille le compteur de clients
+    patients--;                 // Sort de la clinique
     mut.unlock();               // Libère le compteur de clients
 }
 
-void cutHair()
+void takeCareOfPatient()
 {
     cout << "Médecin : Attend un patient" << endl;
 
-    if (customer.try_lock() == false) {
+    if(patient.try_lock() == false) { // Si il n'y a personne dans la file d'attente
         cout << "Médecin : S'endort car il n'y a pas de patient" << endl;
-        barberAsleep = true;        // Indique que le barbier est endormi
-        while (barberAsleep) {      // Attendre jusqu'à ce qu'il soit réveillé
-            //this_thread::yield();   // Laisse d'autres threads fonctionner
-            sleep(1);
+        medicAsleep = true;        // Indique que le barbier est endormi
+        while (medicAsleep) {      // Attendre jusqu'à ce qu'il soit réveillé
         }
         cout << "Médecin : Se fait réveiller" << endl;
     }
-
-    barber.unlock();            // Déclare qu'il est disponible
-    customer.lock();            // Attend un client
-    // Coupe les cheveux        // Coupe les cheveux du client
-    customerDone.lock();        // Attend que le client déclare qu'il a fini
-    barberDone.unlock();        // Termine son travail
+    medic.unlock();             // Déclare qu'il est disponible
+    patient.lock();             // Attend un patient
+                                // Consultation en cours ...
+    patientDone.lock();         // Attend que le patient déclare qu'il a fini
+    medicDone.unlock();         // Termine son travail
 }
 
 void routine()
 {
-    barberAsleep = true;
+    medicWakeUp.unlock();
     while (true)
     {
-        cutHair();
+        takeCareOfPatient();
     }
 }
 
 int main()
 {
-    thread barber(routine);
+    thread medic(routine);
     vector<thread> patientsThreads;
     int clientId = 1;
 
     while (true) {
-        cout << "Nombre de patients dans la clinique :"<< customers << endl;
-        patientsThreads.push_back(thread(getHairCut, clientId++));
+        cout << "Nombre de patients dans la clinique :"<< patients << endl;
+        patientsThreads.push_back(thread(visitClinic, clientId++));
         sleep(chrono_newPatient);
     }
 
